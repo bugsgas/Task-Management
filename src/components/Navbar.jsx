2330cbc60ca203d21be3b7c8ce62d8c4fd309a29
Macintosh <macintosh@Macintoshs-MacBook-Air.local>
2024-02-14 23:56:04 +0700

    add icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { useAuthContext } from '../hooks/useAuthContext'
 
 // styles & images
 import './Navbar.css'
-import Temple from '../assets/logo.png'
+import Temple from '../assets/memo.png'
 
 
 export default function Navbar() {
